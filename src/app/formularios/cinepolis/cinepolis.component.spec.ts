e912b4005351689b1efcb240f4300398421b0cd4
IEVN1003-21000385 <21000385@alumnos.utleon.edu.mx>
2025-09-26 18:00:18 -0600

    CINEPOLIS

--- src/app/formularios/cinepolis/cinepolis.component.spec.ts
+++ src/app/formularios/cinepolis/cinepolis.component.spec.ts
@@ -1,5 +1,5 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
 
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { CinepolisComponent } from './cinepolis.component';
 
 describe('CinepolisComponent', () => {
